a0fc9047942de18bc84d222c352a83c4349d67b9
Massimiliano Baldo <bmaxium01@gmail.com>
2025-06-06 14:48:13 +0200

    refacotr: remove useless trait and reimplet entity_store (SWE is bullshit)

--- src/entity_store/factory.rs
+++
@@ -1,6 +0,0 @@
-use crate::entity_store::api::EntityStore;
-use crate::entity_store::implementation::InMemoryEntityStore;
-
-pub fn create_entity_store() -> Box<dyn EntityStore> {
-    Box::new(InMemoryEntityStore::new())
-}
