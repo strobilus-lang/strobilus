4148a2b30c5c50effcec67fee653fb3d70cc2f62
Massimiliano Baldo <bmaxium01@gmail.com>
2025-06-06 14:49:01 +0200

    refactor: main entrypoint for demo

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,20 @@
-use std::{fs::File, path::Path, str::FromStr};
-use cedar_policy::*;
-use clap::Parser;
-
-mod parser;
 mod entity_store;
-mod policy_engine;
-mod authorization;
+mod executor;
 
-/// Simple PoC of Cedar with state
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Entity of request's principal, like User::"pippo"
-    #[arg(short, long)]
-    principal: String,
-
-    /// Entity of request's action, like Action::"read"
-    #[arg(short, long)]
-    action: String,
-
-    /// Entity of request's resource, like Book::"1984"
-    #[arg(short, long)]
-    resource: String,
-
-    /// Path of the policy
-    #[arg(long, default_value = "policy.cedar")]
-    policy_path: String,
-
-    /// Path of the entites
-    #[arg(long, default_value = "entities.json")]
-    entities_path: String,
-
-    // Path of the rules
-    #[arg(long, default_value = "rules.strobilus")]
-    rules_path: String
-}
-
-fn read_policy(filename: impl AsRef<Path>) -> Result<PolicySet, Box<dyn std::error::Error>> {
+/* fn read_policy(filename: impl AsRef<Path>) -> Result<PolicySet, Box<dyn std::error::Error>> {
     let file_content = std::fs::read_to_string(filename)?;
     PolicySet::from_str(&file_content).map_err(Into::into)
-}
+} */
 
-fn read_entities(filename: impl AsRef<Path>) -> Result<Entities, Box<dyn std::error::Error>> {
+/* fn read_entities(filename: impl AsRef<Path>) -> Result<Entities, Box<dyn std::error::Error>> {
     let file = File::open(filename)?;
     Entities::from_json_file(file, None).map_err(Into::into)
-}
-
-fn read_rules(filename: impl AsRef<Path>) -> Result<parser::EffectTable, Box<dyn std::error::Error>> {
-    let file_content = std::fs::read_to_string(filename)?;
-    Ok(parser::parse_strobilus(&file_content))
-}
+} */
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
+    //let args = Args::parse();
 
-    let policy_set = read_policy(args.policy_path)?;
+/*     let policy_set = read_policy(args.policy_path)?;
     let rules = read_rules(args.rules_path)?;
     println!("{:?}", rules);
 
@@ -78,8 +38,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("-- Associate event: {:?}", policy.annotation("evt"));
         }
     }
+ 
+    println!("Final decision {:?}", answer.concretize().decision()); */
 
-    println!("Final decision {:?}", answer.concretize().decision());
+    //let policy_set = read_policy(args.policy_path)?;
+    //let rules = read_rules(args.rules_path)?;
+
+    /* let action = args.action;
+    let principal = args.principal;
+    let resource = args.resource; */
+
+    //let entities = read_entities(args.entities_path)?;
+
+    // TODO: Refactor Authorizer to use cedar_policy_core crate.
+    //let authorizer = authorization::Authorizer::new(policy_set, entities);
+
+    let data = r#"
+            [
+                {
+                    "uid": {
+                        "type": "User",
+                        "id": "max"
+                    },
+                    "attrs": {
+                        "counter": 10
+                    },
+                    "parents": []
+                }
+            ]
+        "#;
+
+    let mut executor= executor::Executor::with_entity_store(data)?;
+
+    println!("--- Entity store BEFORE: {:?}", executor.clone().entity_store());
+
+    let command = executor::commands::update_attribute_command_1();
+    //let command = executor::commands::update_attribute_command_2();
+
+    let _ = &executor.execute::<()>(command)?;
+
+    let es = &executor.entity_store();
+    println!("--- Entity store AFTER: {:?}", es);
 
     Ok(())
 }
