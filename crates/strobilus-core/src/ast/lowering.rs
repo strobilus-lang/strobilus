50fe5195dc7ef2a796a1333e401e7c2395268f87
Massimiliano Baldo <bmaxium01@gmail.com>
2025-07-10 14:49:46 +0200

    fix: correct lowering in ast

--- crates/strobilus-core/src/ast/lowering.rs
+++ crates/strobilus-core/src/ast/lowering.rs
@@ -1,36 +1,48 @@
+use cedar_policy_core::{ast::{self, ExprBuilder}, parser::cst};
+
 use crate::{
     ast::{command::CommandKind, Command as AstCommand, CommandSet as AstCommandSet},
     parser::cst::{Command as CstCommand, CommandSet as CstCommandSet, Node},
 };
 
+trait ToAstExpr {
+    fn to_ast_expr(&self) -> Result<ast::Expr, Box<dyn std::error::Error>>;
+}
+
+impl ToAstExpr for Node<cst::Expr>{
+    fn to_ast_expr(&self) -> Result<ast::Expr, Box<dyn std::error::Error>> {
+        Ok(self.to_expr::<ExprBuilder<()>>()?)
+    }
+}
+
 fn lower_command(command: Node<CstCommand>) -> Result<AstCommand<()>, Box<dyn std::error::Error>> {
     // TODO: handle better errors
     match command.node.expect("Error parsing command") {
         CstCommand::AddParent(expr1, expr2) => Ok(AstCommand {
-            kind: CommandKind::AddParent(expr1.to_expr()?, expr2.to_expr()?),
+            kind: CommandKind::AddParent(expr1.to_ast_expr()?, expr2.to_ast_expr()?),
         }),
         CstCommand::RemoveParent(expr1, expr2) => Ok(AstCommand {
-            kind: CommandKind::RemoveParent(expr1.to_expr()?, expr2.to_expr()?),
+            kind: CommandKind::RemoveParent(expr1.to_ast_expr()?, expr2.to_ast_expr()?),
         }),
         CstCommand::UpdateEntity(uid, attributes, ancestors, tags) => {
             Ok(AstCommand {
                 kind: CommandKind::UpdateEntity(
-                    uid.to_expr()?,
-                    attributes.to_expr()?,
-                    ancestors.to_expr()?,
-                    tags.to_expr()?,
+                    uid.to_ast_expr()?,
+                    attributes.to_ast_expr()?,
+                    ancestors.to_ast_expr()?,
+                    tags.to_ast_expr()?,
                 ),
             })
         },
         CstCommand::RemoveEntity(uid) => {
             Ok(AstCommand {
-                kind: CommandKind::RemoveEntity(uid.to_expr()?),
+                kind: CommandKind::RemoveEntity(uid.to_ast_expr()?),
             })
         },
         CstCommand::UpdateAttribute(expr1, attr, expr2) => {
             Ok(AstCommand {
                 kind: CommandKind::UpdateAttribute(
-                    expr1.to_expr()?,
+                    expr1.to_ast_expr()?,
                     // Sporcizia totale, vengono rimosse le virgolette
                     // che il parser, nonostante le regole, continua a mettere
                     {
@@ -38,14 +50,14 @@ fn lower_command(command: Node<CstCommand>) -> Result<AstCommand<()>, Box<dyn st
                         _attr[1.._attr.len() - 1].to_owned()
                     },
                     //attr.to_string(),
-                    expr2.to_expr()?,
+                    expr2.to_ast_expr()?,
                 ),
             })
         }
         CstCommand::RemoveAttribute(expr1, attr) => {
             Ok(AstCommand {
                 kind: CommandKind::RemoveAttribute(
-                    expr1.to_expr()?,
+                    expr1.to_ast_expr()?,
                     // Uguale a sopra
                     {
                         let _attr = attr.node.expect("Attribute string missing").to_string();
@@ -62,7 +74,7 @@ fn lower_command(command: Node<CstCommand>) -> Result<AstCommand<()>, Box<dyn st
         }),
         CstCommand::IfThenElse(condition, c1, c2) => Ok(AstCommand {
             kind: CommandKind::IfThenElse(
-                condition.to_expr()?,
+                condition.to_ast_expr()?,
                 Box::new(lower_command(*c1)?),
                 Box::new(lower_command(*c2)?),
             ),
