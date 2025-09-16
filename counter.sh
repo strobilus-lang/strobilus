4228df8377225d415a87c3eed9cef9c225fb0f36
Massimiliano Baldo <bmaxium01@gmail.com>
2025-09-16 09:20:38 +0200

    chore: moved example in strobilus crate

--- counter.sh
+++ counter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cargo run -- -p 'User::"max"' -a 'Action::"view"' -r 'File::"42"' --policy-path policy.cedar --rules-path rules.strobilus
-cargo run --example counter -p strobilus-core
+cargo run --example counter -p strobilus
