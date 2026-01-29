f0636e922db77d1563836ec32047b5b3515eb6e8
Massimiliano Baldo <bmaxium01@gmail.com>
2026-01-29 16:03:40 +0100

    chore: add basic documentation

--- crates/strobilus-core/src/parser/cst.rs
+++ crates/strobilus-core/src/parser/cst.rs
@@ -1,8 +1,11 @@
+//! # Concrete Syntax Tree 
+//! It's the first representation of Strobilus program
 use cedar_policy_core::parser::cst::{Expr, Str};
 use cedar_policy_core::parser::Node as CedarNode;
 
 pub(crate) type Node<N> = CedarNode<Option<N>>;
 
+/// These are the possible forms of Strobilus commands
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Command {
     // Basic commands
@@ -18,6 +21,8 @@ pub enum Command {
     Skip,
 }
 
+/// This structure represents the entrypoint of
+/// Strobilus programs
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CommandSet {
     pub on_allow: Box<Node<Command>>,
@@ -25,10 +30,12 @@ pub struct CommandSet {
 }
 
 impl CommandSet {
+    /// Returns the commands for ALLOW decisions
     pub fn on_allow(&self) -> Node<Command> {
         *self.on_allow.clone()
     }
 
+    /// Returns the commands for DENY decisions
     pub fn on_deny(&self) -> Node<Command> {
         *self.on_deny.clone()
     }    
--- crates/strobilus-core/src/parser/mod.rs
+++ crates/strobilus-core/src/parser/mod.rs
@@ -1,3 +1,5 @@
+//! # Parser
+//! This is a collection of functions for parse a Strobilus set of commands
 use std::sync::Arc;
 
 use cedar_policy_core::parser::Node;
@@ -8,13 +10,15 @@ mod err;
 
 lalrpop_mod!(grammar);
 
-pub fn parse_command(input: &str) -> Result<Node<Option<cst::Command>>, err::RawParseError> {
+/// Parse a single Strobilus command
+pub fn parse_command(input: &str) -> Result<Node<Option<cst::Command>>, err::RawParseError<'_>> {
     let parser = grammar::CommandParser::new();
     let mut errors = Vec::new();
     parser.parse(&mut errors, &Arc::from(input), input)
 }
 
-pub fn parse_command_set(input: &str) -> Result<Node<Option<cst::CommandSet>>, err::RawParseError> {
+/// Parse a set of Strobilus commands
+pub fn parse_command_set(input: &str) -> Result<Node<Option<cst::CommandSet>>, err::RawParseError<'_>> {
     let parser = grammar::CommandSetParser::new();
     let mut errors = Vec::new();
     parser.parse(&mut errors, &Arc::from(input), input)
